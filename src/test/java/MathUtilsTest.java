32e86125469203349d0a1f1d7e0fc273349ebf04
Do Anh Tu <tulaanh0317@gmail.com>
2026-04-28 15:22:49 +0700

    init project

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,14 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.example.MathUtils;
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+    MathUtils math = new MathUtils();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, math.add(2, 3));
+    }
+}
